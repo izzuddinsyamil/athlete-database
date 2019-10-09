4a2e4198ded859845e21a5bf97f70e04d5bae50e
syamil <izzuddinsyamil1@gmail.com>
2019-10-09 14:52:30 +0700

    remove bootstrap js files

--- athletedb/templatetags/templatehelpers.py
+++ athletedb/templatetags/templatehelpers.py
@@ -6,7 +6,6 @@ register = template.Library()
 def relative_url(value, field_name, urlencode=None):
     url = '?{}={}'.format(field_name, value)
 
-    print('\n helper url : ', urlencode)
     if urlencode:
         querystring = urlencode.split('&')
         filtered_querystring = filter(lambda p: p.split('=')[0] != field_name, querystring)
