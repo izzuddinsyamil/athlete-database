c4477a92ec5ae436d4b3724f0b06c4a976cd3cce
M. Izzuddin Syamil <muhammad.syamil@amartha.com>
2020-01-05 17:01:23 +0700

    update ui athlete detail

--- athletedb/static/athletedb/js/athlete-detail.js
+++ athletedb/static/athletedb/js/athlete-detail.js
@@ -9,6 +9,7 @@ function getInitialdata(id) {
     $.get(url, function (data, status) {
         if (status == 'success') {
             processSportsData(data["sports"]);
+            processAchievementData(data["achievements"]);
         } else {
             console.log('failed to get data from ', url);
         }
@@ -16,13 +17,45 @@ function getInitialdata(id) {
 }
 
 function processSportsData(sportList) {
-    var sports = '<td>';
+    var sports = '';
     if (sportList.length > 0) {
         sportList.forEach(function (element) {
             sports += element.name + ', ';
         });
-        sports = sports.substring(0, sports.length - 2) + '</td>';
+        sports = sports.substring(0, sports.length - 2);
     }
-    sports += '</td>';
-    $('.sports-row').append(sports);
+
+    var sportData = `
+    <dd class="col-sm-9">` + sports + `</dd>`;
+    $('.athlete-bio-detail').append(sportData);
+
+}
+
+function processAchievementData(achievementList) {
+    achievementList.forEach(function (element) {
+        var achievementID = `achievement-` + element.id;
+        var achievementTitle = element.result + `, ` + element.level;
+
+        var certificateImg = `<p class="font-italic">Tidak ada file sertifikat</p>`;
+        if (element.certificate_file != null) {
+            certificateImg = `<img src="` + element.certificate_file + `" class="achievement-img" width="500" height="300"></img>`
+        }
+
+        var achievementData = `
+        <div class="achievement-per-athlete shadow-sm p-4 mb-4 bg-white rounded" id="` + achievementID +`">
+            <div class="achievement-title border-bottom mb-3">
+                <h5>`+ achievementTitle +`</h5>
+            </div>
+            <div class="achievement-body">
+                <div class="achievement-desc mt-1 mb-3">` + element.description + `</div>
+                <div class="achievement-certificate mb-3">
+                    sertifikat : <br>
+                    ` + certificateImg + `
+                </div>
+            </div>
+        </div>
+        `;
+
+        $(".achievement-list").append($(achievementData));
+    });
 }
